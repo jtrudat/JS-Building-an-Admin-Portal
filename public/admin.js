38e3c8c5cac028b7236cff47c70a7b20f0dc7368
Erik Tatro <eriktatro@gmail.com>
2022-06-03 19:06:41 -0700

    fetch

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,45 @@
 
 // Your Code Here
+
+async function main() {
+
+    let response = await fetch('http://127.0.0.1:3001/listBooks')
+
+    let books = await response.json()
+
+    books.forEach(renderBook)
+}
+
+function renderBook(book) {
+    let root = document.querySelector('#root')
+
+    let li = document.createElement('li')
+    li.textContent = book.title
+
+    let quantityInput = document.createElement('input')
+    quantityInput.value = book.quantity
+
+    let saveButton = document.createElement('button')
+    saveButton.textContent = 'Save & Update',
+    saveButton.style.color = 'blue',
+    saveButton.style.backgroundColor = 'yellow',
+
+    saveButton.addEventListener('click', function() {
+        fetch('http://127.0.0.1:3001/updateBook', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: book.id,
+                quantity: quantityInput.value
+            })
+        })
+    })
+
+    li.append(quantityInput, saveButton)
+
+    root.append(li)
+}
+
+main();
